6db89a90bb0134559af8804d2e39f10d53703246
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2010-05-03 14:23:37 -0300

    Oops, I should't have added this file

--- lib/vault/storage/lint.rb
+++
@@ -1,9 +0,0 @@
-module Vault
-  module Storage
-    module Lint
-      def test_implements_getter
-        assert model_klass.respond_to?(:[]), "#{model_klass} should respond to #[]"
-      end
-    end
-  end
-end
