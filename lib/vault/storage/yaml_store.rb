e6d8cb0c0aae243f444ebc45bdd4c5ec99405d8e
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2010-05-03 17:03:36 -0300

    Clean up the implementation of YamlStore a bit

--- lib/vault/storage/yaml_store.rb
+++ lib/vault/storage/yaml_store.rb
@@ -8,30 +8,39 @@ module Vault
 
       delegate :[], :[]=, :size, :each, :delete, :to => :doc
 
-      def initialize(file=nil, contents=ActiveSupport::OrderedHash.new)
+      def initialize(file=nil)
         @file = file
+        at_exit { flush if @file.present? }
+      end
 
-        if @file.nil?
-          @doc = contents
-        else
-          at_exit { flush }
-        end
+      def initialize_copy(*)
+        @file = nil
       end
 
       def filter(query)
-        filtered = doc.inject(ActiveSupport::OrderedHash.new) do |result, (key, properties)|
+        results = doc.inject(ActiveSupport::OrderedHash.new) do |result, (key, properties)|
           result[key] = properties if Set.new(properties).superset?(Set.new(query))
           result
         end
 
-        self.class.new(nil, filtered)
+        filtered_copy(results)
       end
 
       def flush
         File.open(@file, "w+") {|f| YAML.dump(@doc, f) }
       end
 
+      protected
+
+      def doc=(doc)
+        @doc = doc
+      end
+
       private
+
+      def filtered_copy(doc)
+        dup.tap {|store| store.doc = doc }
+      end
 
       def doc
         @doc ||= ActiveSupport::OrderedHash.new(YAML.load_file(@file))
